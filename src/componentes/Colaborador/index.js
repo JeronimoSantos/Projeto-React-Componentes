9228c01a9144d3cce20b3da93fd1beec0cd10809
Jeronimo Santos <149694502+JeronimoSantos@users.noreply.github.com>
2024-07-14 23:03:00 -0300

    Add files via upload

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,39 @@
+import { FaWindowClose } from "react-icons/fa";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import './colaborador.css'
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, onFavoritar }) => {
+    function favoritar () {
+        onFavoritar(colaborador.id)
+    }
+
+    const propsfavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
+    return (<div className="colaborador">
+        <FaWindowClose 
+            size={25} 
+            className='deletar' 
+            onClick={() => aoDeletar(colaborador.id)}
+
+        />
+        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+            <img src={colaborador.imagem} alt={colaborador.nome} />
+        </div>
+        <div className="rodape">
+            <h4>{colaborador.nome}</h4>
+            <h5>{colaborador.cargo}</h5>
+            <div className="favoritar">
+                {colaborador.favorito 
+                    ? <AiFillHeart {...propsfavorito} color="#ff0000"/> 
+                    : <AiOutlineHeart {...propsfavorito}/>
+                }
+
+            </div>
+        </div>
+    </div>)
+}
+
+export default Colaborador
